4583da109043b4400ed4fc9b75658343527ea740
sog-spirit <78286856+sog-spirit@users.noreply.github.com>
2021-08-26 09:58:45 +0700

    Câu 2: Tạo bảng

--- src/cau2/Cau2.java
+++ src/cau2/Cau2.java
@@ -9,6 +9,7 @@ public class Cau2 {
 	public static void main(String[] args) {
 		try {
 			createDatabase();
+			createTable();
 		}
 		catch (Exception e) {
 			System.out.println(e);
@@ -16,7 +17,7 @@ public class Cau2 {
 	}
 	
 	public static Connection getConnection() throws Exception {
-		String connectionString = "jdbc:sqlserver://localhost; integratedSecurity=true";
+		String connectionString = "jdbc:sqlserver://localhost; database=dbTourDuLich; integratedSecurity=true";
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			Connection connection = DriverManager.getConnection(connectionString);
@@ -24,6 +25,7 @@ public class Cau2 {
 			return connection;
 		}
 		catch(Exception e) {
+			System.out.println("Ket noi den may chu khong thanh cong");
 			System.out.println(e);
 		}
 		return null;
@@ -31,10 +33,10 @@ public class Cau2 {
 	
 	public static void createDatabase() throws Exception {
 		try {
-			Connection connection = getConnection();
-			PreparedStatement createDatabase = connection.prepareStatement("CREATE DATABASE dbTourDuLich");
+			String connectionString = "jdbc:sqlserver://localhost; integratedSecurity=true";
+			Connection connection = DriverManager.getConnection(connectionString);
+			PreparedStatement createDatabase = connection.prepareStatement("IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = dbTourDuLich) CREATE DATABASE dbTourDuLich");
 			createDatabase.executeUpdate();
-			System.out.println("Tao co so du lieu thanh cong");
 		}
 		catch(Exception e) {
 			System.out.println();
@@ -42,6 +44,23 @@ public class Cau2 {
 	}
 	
 	public static void createTable() throws Exception {
-		
+		try {
+			Connection connection = getConnection();
+			PreparedStatement createTable = connection.prepareStatement(""
+					+ "IF NOT EXISTS (select * from sysobjects where name='THANHVIEN' and xtype='U')"
+					+ "CREATE TABLE THANHVIEN ("
+					+ "MaThanhVien char(8) primary key,"
+					+ "TenThanhVien char(100),"
+					+ "NgaySinh date,"
+					+ "DiaChi char(100),"
+					+ "Email char(50),"
+					+ "SoDienThoai char(15),"
+					+ "ChiPhiNhan float)");
+			createTable.executeUpdate();
+			System.out.println("Tao bang thanh cong");
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 }
